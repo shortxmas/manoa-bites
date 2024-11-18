95b33b796d4cee327426f19a846ec262c306a20d
pelitaf <pelita.felicitas@gmail.com>
2024-11-18 13:05:12 -1000

    change footer container size

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,16 +1,48 @@
-import { Col, Container } from 'react-bootstrap';
+import { Button, Col, Container, Row } from 'react-bootstrap';
 
-/** The Footer appears at the bottom of every page. Rendered by the App Layout component. */
 const Footer = () => (
-  <footer className="mt-auto py-3 bg-light">
+  <footer className="footer mt-auto py-3 bg-success text-white">
     <Container>
-      <Col className="justify-content-end">
-        <h3>Resource Links:</h3>
-        <p>FAQ</p>
-        <p>About Us</p>
-        <p>Report a problem</p>
-        <p><a href="https://github.com/manoa-bites">Link to our source code</a></p>
-      </Col>
+      <Row className="align-items-start justify-content-between">
+        {/* Left Column - Manoa Bites */}
+        <Col md={3}>
+          <h3>MANOA BITES:</h3>
+          <p>
+            This application is designed to help students and staff at the University of Hawaii at Manoa
+            easily find food options across campus locations.
+          </p>
+        </Col>
+
+        {/* Right Column - Links */}
+        <Col md={4} className="text-md-start">
+          <h3>Links:</h3>
+          <ul className="list-unstyled">
+            <li><a href="foo" className="text-white">FAQ</a></li>
+            <li><a href="/issueform" className="text-white">Report a Problem</a></li>
+            <li>
+              <a href="https://github.com/manoa-bites/manoa-bites" className="text-white">
+                Link to our GitHub repo
+              </a>
+            </li>
+          </ul>
+        </Col>
+      </Row>
+
+      {/* Optional CTA Section */}
+      <Container className="my-4">
+        <hr className="mb-4" style={{ borderColor: 'white' }} />
+        <Row className="justify-content-center align-items-center text-center">
+          <Col xs="auto">
+            <span className="me-3">Register for free</span>
+          </Col>
+          <Col xs="auto">
+            <Button variant="dark" className="rounded-pill" href="\auth\signin">
+              Sign up!
+            </Button>
+          </Col>
+        </Row>
+        <hr className="mt-4" style={{ borderColor: 'white' }} />
+      </Container>
     </Container>
   </footer>
 );
