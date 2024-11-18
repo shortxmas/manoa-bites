ba85011f56a20447b86503cc9f15fa5e1895151f
pelitaf <pelita.felicitas@gmail.com>
2024-11-18 07:59:08 -1000

    works on local deployment but not on vercel

--- src/app/issueform/page.tsx
+++ src/app/issueform/page.tsx
@@ -1,7 +1,7 @@
 import { getServerSession } from 'next-auth';
 import authOptions from '@/lib/authOptions';
 import { loggedInProtectedPage } from '@/lib/page-protection';
-import IssueForm from '@/components/AddStuffForm';
+import IssueForm from '@/components/IssueForm';
 
 const ReportAProblem = async () => {
   // Protect the page, only logged in users can access it.
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,23 +4,12 @@ import { Col, Container } from 'react-bootstrap';
 const Footer = () => (
   <footer className="mt-auto py-3 bg-light">
     <Container>
-<<<<<<< Updated upstream
-      <Col className="text-center">
-        Department of Information and Computer Sciences
-        <br />
-        University of Hawaii
-        <br />
-        Honolulu, HI 96822
-        <br />
-        <a href="http://ics-software-engineering.github.io/nextjs-application-template">Template Home Page</a>
-=======
       <Col className="justify-content-end">
         <h3>Resource Links:</h3>
         <p>FAQ</p>
         <p>About Us</p>
         <p><a href="/issueform">Report a problem</a></p>
         <p><a href="https://github.com/manoa-bites">Link to our source code</a></p>
->>>>>>> Stashed changes
       </Col>
     </Container>
   </footer>
