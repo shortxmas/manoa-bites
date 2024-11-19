99e401a028f06b09de4bd977091a6f976470fcaa
pelitaf <pelita.felicitas@gmail.com>
2024-11-18 14:07:14 -1000

    eslint error fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import { Container, Image } from 'react-bootstrap';
 const Home = () => (
   <main>
     <Container id="landing-page" fluid className="py-3">
-      <Image src="manoabiteslogo.jpg" fluid style={{ height: '500px' }}/>
+      <Image src="manoabiteslogo.jpg" fluid style={{ height: '500px' }} />
       <h2>Welcome To Manoa Bites</h2>
     </Container>
   </main>
