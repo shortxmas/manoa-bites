a8bcb0a35f7c76218ae7d7f87af3ff6360f08c38
pelitaf <pelita.felicitas@gmail.com>
2024-11-18 14:14:27 -1000

    adjustments for landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,13 @@ import { Container, Image } from 'react-bootstrap';
 const Home = () => (
   <main>
     <Container id="landing-page" fluid className=" justify-content-center py-3">
-      <Image src="FullLogo.png" fluid style={{ height: '500px' }} />
+      <Container className="d-flex justify-content-center align-items-center">
+        <Image src="/FullLogo.png" alt="Manoa Bites" style={{ height: '500px', borderRadius: '50%' }} />
+        <span style={{ fontSize: '24px', marginLeft: '10px', color: 'black' }}>Manoa Bites</span>
+      </Container>
+      <Container className="d-flex justify-content-center align-items-center">
+        <h2>Find your favorite food spots on campus!</h2>
+      </Container>
     </Container>
   </main>
 );
